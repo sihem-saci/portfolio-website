a24ecb6513806eb3821e5cb45c6a1914e43e8d81
sihem-saci <134804435+sihem-saci@users.noreply.github.com>
2023-07-09 14:12:55 +0100

    Update portfolio.js

--- portfolio.js
+++ portfolio.js
@@ -1,13 +1,26 @@
+const header= document.getElementById("header");
 const hamburger= document.querySelector("#header .nav-list .hamburger");
-
 const mobile_menu= document.querySelector(" #header .nav-list ul");
 
-
-const header= document.querySelector(" #header");
-
 const faders= document.querySelector('#services ');
+const sectionOne = document.getElementById('main');
 
-
+const sectionOneOptions = {
+    rootMargin: "0px 0px 0px 0px"
+};
+const sectionOneObserver = new IntersectionObserver(function(entries, sectionOneObserver) {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting) {
+           header.classList.add("nav-scrolled"); 
+        
+        }
+        else {
+            header.classList.remove("nav-scrolled");
+        }
+    });
+}, sectionOneOptions);
+sectionOneObserver.observe(sectionOne);
+// triggering the hamburger menu to open the NavBar
 hamburger.addEventListener('click', () => { 
 hamburger.classList.toggle('active');
 mobile_menu.classList.toggle('active');
